4eeb4f52fd3166843df2b23d8efa22578a0de34e
Ujjwal Chafle <ujjwalchafle1@gmail.com>
2021-08-09 13:53:59 +0100

    Update code comments.

--- DILib/DIContainer.swift
+++ DILib/DIContainer.swift
@@ -21,11 +21,25 @@ public class DIContainer {
 }
 
 extension DIContainer: DIContainerProtocol {
+  
+  /// This API is used to register the dependency for a given specific type with an unique idetifier.
+  /// Unique identifier is used to allow registration of multiple instances for the same type.
+  /// - Parameters:
+  ///   - type: Type of the instance to register dependency
+  ///   - component: Instance to register
+  ///   - identifier: Unique identifier for the instance
     public func register<Component>(type: Component.Type,
                                     component: Any, identifier: String) {
         components["\(identifier)"] = component
     }
     
+  
+  /// This API is used to resolve the dependency for a given specific type with an unique idetifier.
+  /// Unique identifier is used to allow resolve multiple instances for the same type.
+  /// - Parameters:
+  ///   - type: Type of the instance to resolve dependency
+  ///   - identifier: Unique identifier for the instance used while registration
+  /// - Returns: Instance of the given type
     public func resolve<Component>(type: Component.Type,
                                    identifier: String) -> Component? {
         
@@ -33,11 +47,19 @@ extension DIContainer: DIContainerProtocol {
         return components["\(identifier)"] as? Component
     }
     
+  /// This API is used to register the dependency for a given specific type.
+  /// - Parameters:
+  ///   - type: Type of the instance to register
+  ///   - component: Instance to register
     public func register<Component>(type: Component.Type,
                                     component: Any) {
         components["\(type)"] = component
     }
     
+  /// This API is used to resolve the dependency for a given specific type.
+  /// - Parameters:
+  ///   - type: Type of the instance to resolve dependency
+  /// - Returns: Instance of the given type
     public func resolve<Component>(type: Component.Type) -> Component? {
         precondition(components["\(type.self)"] != nil, "No Dependency found for - \(Component.self), Application must register a dependency befoe resoving it.")
         return components["\(type)"] as? Component
